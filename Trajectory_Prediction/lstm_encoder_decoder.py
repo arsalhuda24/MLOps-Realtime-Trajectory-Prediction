b23c98751fed44b9b5610711420c136358bd24f7
Arsal Syed <arsalhuda@gmail.com>
2022-07-13 22:54:24 -0700

    Add files via upload
    model

--- Trajectory_Prediction/lstm_encoder_decoder.py
+++ Trajectory_Prediction/lstm_encoder_decoder.py
@@ -1,41 +1,140 @@
-import tensorflow as tf
-from keras.layers import LSTM, Dense, RepeatVector, Dropout, TimeDistributed
-from keras import optimizers
-from keras.models import Sequential
-# from sgan.data.trajectories import TrajectoryDataset
 import matplotlib.pyplot as plt
-from keras.models import load_model
-import torch
-import os
-# from sgan.losses import displacement_error, final_displacement_error
-from keras.layers import embeddings
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-os.chdir("/home/asyed/my_docker/Trajectory_prediction/sgan")
-# from sgan.data.trajectories import TrajectoryDataset, seq_collate
-from preprocess import TrajectoryDataset
+import random
+import os, errno
+import sys
+from tqdm import trange
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+import torch
+import time
 
-from keras import optimizers
-from tensorflow import keras
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+class lstm_encoder(nn.Module):
+    ''' Encodes time-series sequence '''
+
+    def __init__(self, input_size, hidden_size,emb_dim,num_layers = 1):
+        
+        '''
+        : param input_size:     the number of features in the input X
+        : param hidden_size:    the number of features in the hidden state h
+        : param num_layers:     number of recurrent layers (i.e., 2 means there are
+        :                       2 stacked LSTMs)
+        '''
+        
+        super(lstm_encoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.emb_dim = emb_dim
+
+        # define LSTM layer
+        
+        
+        self.embedding = nn.Linear(input_size, emb_dim)
+        self.lstm = nn.LSTM(input_size = emb_dim, hidden_size = hidden_size,
+                             num_layers = num_layers)
+        
+#         self.lstm = nn.LSTM(input_size = input_size, hidden_size = hidden_size,
+#                             num_layers = num_layers)
+
+    def forward(self, x_input):
+        
+        '''
+        : param x_input:               input of shape (seq_len, # in batch, input_size)
+        : return lstm_out, hidden:     lstm_out gives all the hidden states in the sequence;
+        :                              hidden gives the hidden state and cell state for the last
+        :                              element in the sequence 
+        '''
+        
+        x_input = self.embedding(x_input)
+#         print("CHECK INPUT SHAPE", x_input.shape)
+        
+#         lstm_out, self.hidden = self.lstm(x_input.view(x_input.shape[0], x_input.shape[1], self.input_size))
+        lstm_out, self.hidden = self.lstm(x_input.view(x_input.shape[0], x_input.shape[1], self.emb_dim))
+
+        
+        return lstm_out, self.hidden     
+    
+    def init_hidden(self, batch_size):
+        
+        '''
+        initialize hidden state
+        : param batch_size:    x_input.shape[1]
+        : return:              zeroed hidden state and cell state 
+        '''
+        
+        eturn (torch.zeros(self.num_layers, batch_size, self.hidden_size),
+                torch.zeros(self.num_layers, batch_size, self.hidden_size))
 
 
+class lstm_decoder(nn.Module):
+    ''' Decodes hidden state output by encoder '''
+    
+    def __init__(self, input_size, hidden_size, num_layers = 1):
 
-hidden_neurons= 128
-tf.config.run_functions_eagerly(True)
+        '''
+        : param input_size:     the number of features in the input X
+        : param hidden_size:    the number of features in the hidden state h
+        : param num_layers:     number of recurrent layers (i.e., 2 means there are
+        :                       2 stacked LSTMs)
+        '''
+        
+        super(lstm_decoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_size = input_size, hidden_size = hidden_size,
+                            num_layers = num_layers)
+        self.linear = nn.Linear(hidden_size, input_size)           
+
+    def forward(self, x_input, encoder_hidden_states):
+        
+        '''        
+        : param x_input:                    should be 2D (batch_size, input_size)
+        : param encoder_hidden_states:      hidden states
+        : return output, hidden:            output gives all the hidden states in the sequence;
+        :                                   hidden gives the hidden state and cell state for the last
+        :                                   element in the sequence 
+ 
+        '''
+        
+        lstm_out, self.hidden = self.lstm(x_input.unsqueeze(0), encoder_hidden_states)
+        output = self.linear(lstm_out.squeeze(0))     
+        
+        return output, self.hidden
+
+class lstm_seq2seq(nn.Module):
+    ''' train LSTM encoder-decoder and make predictions '''
+    
+#     def __init__(self, input_size, emb_dim ,hidden_size):
+    def __init__(self, input_size,hidden_size, emb_dim):
 
 
-model= Sequential()
-model.add(Dense(64, input_shape=(None,2),activation="linear"))
-model.add(LSTM(128, input_shape=(8,64), return_sequences= False))
-model.add(RepeatVector(12))
-model.add(LSTM(128, return_sequences= True))
-model.add(Dropout(0.3))
-model.add(TimeDistributed(Dense(2,activation= "linear")))
-#optim= optimizers.adam_v2(lr=0.001)
-optim = keras.optimizers.Adam(learning_rate=0.001)
+        '''
+        : param input_size:     the number of expected features in the input X
+        : param hidden_size:    the number of features in the hidden state h
+        '''
 
-
-model.compile(loss="mse", optimizer=optim, metrics=["accuracy"])
-
-
+        super(lstm_seq2seq, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.emb_dim = emb_dim
+        self.encoder = lstm_encoder(input_size = input_size, hidden_size = hidden_size, emb_dim = emb_dim)
+        self.decoder = lstm_decoder(input_size = input_size, hidden_size = hidden_size)
+           
+    def forward(self, x_input, target_len):
+        batch_size = x_input.shape[1]
+        outputs = torch.zeros(target_len, batch_size, x_input.shape[2])        
+        encoder_output, encoder_hidden = self.encoder(x_input)
+        decoder_input = x_input[-1,:,:]
+        decoder_hidden = encoder_hidden
+        for t in range(target_len):
+            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+            outputs[t] = decoder_output
+            decoder_input = decoder_output           
+        return outputs
